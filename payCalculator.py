c91f34952712e401c0ee9f7583864d72f7fde397
Chad Anderson <Chad.Anderson@vu.com>
2025-01-17 15:44:19 -0600

    optionals arent passing, tried troubleshooting but couldnt figure out why. gives correct result when manually input

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,24 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
+    hrs = input("Enter Hours: ")
+    try:
+        hours = float(hrs)
+    except: 
+        print("Error, please enter numeric input")
+        exit()
+    rte = input("Enter Rate: ")
+    try:
+        rate = float(rte)
+    except: 
+        print("Error, please enter numeric input")
+        exit()
+    pay = hours*rate
+    if hours > 40:
+        OT_pay = (hours-40.0)*(rate*0.5)
+        pay = pay+OT_pay
+        print ("Pay:", pay)
+    else:
+        print ("Pay:", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
